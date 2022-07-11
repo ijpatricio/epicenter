3e938f7386b4d77079bdd915f1eca19d5af9417c
Joao Patricio <ijpatricio@gmail.com>
2022-07-11 20:05:22 +0100

    Cleanup

---
+++ app/pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+
+      <v-card
+        class="mx-auto"
+        max-width="344"
+      >
+        <v-card-text>
+          <div>Welcome to</div>
+          <p class="text-h4 text--primary">
+            Epicenter
+          </p>
+          <p>adjective</p>
+          <div class="text--primary">
+            the central point of something
+          </div>
+        </v-card-text>
+      </v-card>
+
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
